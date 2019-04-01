43038bd94cedfeb851b172dc918558b80782b28d
Manuel Osvaldo Olguín <molguin@kth.se>
2019-04-01 11:13:34 +0200

    fix license - MIT

--- minisync.h
+++ minisync.h
@@ -1,7 +1,7 @@
 /*
 * Author: Manuel Olguín Muñoz <manuel@olguin.se>
 *
-* Copyright© Manuel Olguín Muñoz 2019
+* Copyright© 2019 Manuel Olguín Muñoz
 * See LICENSE file included in the root directory of this project for licensing and copyright details.
 */
 
