5a7a0deef6d691a822a6d1668c6bc1f7669a4d96
Manuel Osvaldo Olguín <molguin@kth.se>
2019-04-04 11:45:35 +0200

    implementing network nodes

--- src/node.cpp
+++ src/node.cpp
@@ -6,3 +6,27 @@
 */
 
 #include "node.h"
+#include <string.h>
+
+MiniSync::Node::Node(uint16_t bind_port, std::string& peer, uint16_t peer_port) :
+bind_port(bind_port), local_addr(SOCKADDR{}), peer_addr(SOCKADDR{})
+{
+    this->sock_fd = socket(PF_INET, SOCK_DGRAM, IPPROTO_UDP);
+    memset(&this->local_addr, 0, sizeof(this->local_addr));
+    this->local_addr.sin_family = AF_INET;
+    this->local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    this->local_addr.sin_port = htons(bind_port);
+
+    if (bind(this->sock_fd, (struct sockaddr*) &this->local_addr, sizeof(this->local_addr)) < 0)
+    {
+        // TODO: Handle error
+        exit(1);
+    }
+
+    this->peer = peer;
+    this->peer_port = peer_port;
+    memset(&this->peer_addr, 0, sizeof(this->peer_addr));
+    this->peer_addr.sin_family = AF_INET;
+    this->peer_addr.sin_addr.s_addr = inet_addr(peer.c_str());
+    this->peer_addr.sin_port = htons(peer_port);
+}
--- src/node.h
+++ src/node.h
@@ -8,16 +8,38 @@
 #ifndef MINISYNCPP_NODE_H
 #define MINISYNCPP_NODE_H
 
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <string>
+#include "minisync.h"
+
 namespace MiniSync
 {
-    class Node {
+    typedef struct sockaddr_in SOCKADDR;
 
+    class Node
+    {
+    protected:
+        int sock_fd;
+        uint32_t bind_port;
+        SOCKADDR local_addr;
+        SOCKADDR peer_addr;
+        std::string peer;
+        uint32_t peer_port;
+
+        Node(uint16_t bind_port, std::string& peer, uint16_t peer_port);
     };
 
-    class ReferenceNode
+    class ReferenceNode : Node
     {
+    public:
+        ReferenceNode(uint16_t bind_port, std::string& peer, uint16_t peer_port);
+    };
 
-
+    class SyncNode : Node
+    {
+    public:
+        SyncNode(uint16_t bind_port, std::string& peer, uint16_t peer_port, MiniSync::SyncAlgorithm sync_algo);
     };
 }
 
