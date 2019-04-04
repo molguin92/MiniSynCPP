663e995ebaac5d46ce661d1ad59953ae5bbca75f
Manuel Osvaldo Olguín <molguin@kth.se>
2019-04-04 10:14:23 +0200

    add command line parsing

--- main.cpp
+++ main.cpp
@@ -6,9 +6,20 @@
 */
 
 #include <iostream>
+#include "CLI11.hpp"
 
-int main()
+int main(int argc, char* argv[])
 {
-    std::cout << "Hello, World!" << std::endl;
+    bool reference_mode = false;
+    std::string peer = "0.0.0.0";
+    uint32_t port = 1337;
+
+    CLI::App app{"Standalone demo implementation of the Tiny/MiniSync time synchronization algorithms."};
+    app.add_flag("-r,--ref", reference_mode, "Start node in reference mode; "
+                                             "i.e. other peers synchronize to this node's clock.");
+    app.add_option<std::string>("ADDRESS", peer, "Address of peer to synchronize with.", true);
+    app.add_option<uint32_t>("PORT", port, "UDP Port on peer to communicate with.", true);
+
+    CLI11_PARSE(app, argc, argv);
     return 0;
 }
---
+++ node.h
@@ -0,0 +1,18 @@
+/*
+* Author: Manuel Olguín Muñoz <manuel@olguin.se>
+* 
+* Copyright© 2019 Manuel Olguín Muñoz
+* See LICENSE file included in the root directory of this project for licensing and copyright details.
+*/
+
+#ifndef MINISYNCPP_NODE_H
+#define MINISYNCPP_NODE_H
+
+
+class node
+{
+
+};
+
+
+#endif //MINISYNCPP_NODE_H
