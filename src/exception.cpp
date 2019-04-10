bda10af73181dc5c3121340aea4000e78c42f54c
Manuel Olguín Muñoz <molguin@kth.se>
2019-04-10 11:05:48 +0200

    add exceptions

---
+++ src/exception.cpp
@@ -0,0 +1,23 @@
+/*
+* Author: Manuel Olguín Muñoz <manuel@olguin.se>
+* 
+* Copyright© 2019 Manuel Olguín Muñoz
+* See LICENSE file included in the root directory of this project for licensing and copyright details.
+*/
+
+#include "exception.h"
+
+const char* MiniSync::Exceptions::TimeoutException::what() const noexcept
+{
+    return "Operation timed out.";
+}
+
+const char* MiniSync::Exceptions::SocketReadException::what() const noexcept
+{
+    return "Error while trying to read from socket.";
+}
+
+const char* MiniSync::Exceptions::SocketWriteException::what() const noexcept
+{
+    return "Error while trying to write to socket.";
+}
---
+++ src/exception.h
@@ -0,0 +1,38 @@
+/*
+* Author: Manuel Olguín Muñoz <manuel@olguin.se>
+* 
+* Copyright© 2019 Manuel Olguín Muñoz
+* See LICENSE file included in the root directory of this project for licensing and copyright details.
+*/
+
+#ifndef MINISYNCPP_EXCEPTION_H
+#define MINISYNCPP_EXCEPTION_H
+
+#include <exception>
+
+namespace MiniSync
+{
+    namespace Exceptions
+    {
+        class TimeoutException : public std::exception
+        {
+        public:
+            const char* what() const noexcept final;
+        };
+
+        class SocketReadException : public std::exception
+        {
+        public:
+            const char* what() const noexcept final;
+        };
+
+        class SocketWriteException : public std::exception
+        {
+        public:
+            const char* what() const noexcept final;
+        };
+    }
+}
+
+
+#endif //MINISYNCPP_EXCEPTION_H
