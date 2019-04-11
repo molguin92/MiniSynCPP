9e7fd3de50673c0e7c9bf0843e7cac028134378f
Manuel Osvaldo Olguín <molguin@kth.se>
2019-04-11 11:48:25 +0200

    loguru initialization

--- src/main.cpp
+++ src/main.cpp
@@ -7,9 +7,12 @@
 
 #include <iostream>
 #include "CLI11/CLI11.hpp"
+#include "loguru/loguru.hpp"
 
 int main(int argc, char* argv[])
 {
+    loguru::g_stderr_verbosity = loguru::Verbosity_ERROR; // set default verbosity to ERROR or FATAL
+    loguru::init(argc, argv); // parse -v flags
     bool reference_mode = false;
     std::string peer = "0.0.0.0";
     uint32_t port = 1337;
@@ -19,7 +22,7 @@ int main(int argc, char* argv[])
                                              "i.e. other peers synchronize to this node's clock.");
     app.add_option<std::string>("ADDRESS", peer, "Address of peer to synchronize with.", true);
     app.add_option<uint32_t>("PORT", port, "UDP Port on peer to communicate with.", true);
-
-    CLI11_PARSE(app, argc, argv);
+    app.add_option("-v", loguru::g_stderr_verbosity, "Set verbosity level.", true);
+    CLI11_PARSE(app, argc, argv)
     return 0;
 }
