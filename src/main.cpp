589986ccb96f7240fa4b2cf5ded5a90913c644c2
Manuel Osvaldo Olguín <molguin@kth.se>
2019-04-12 17:18:12 +0200

    set up modes for startup

--- src/main.cpp
+++ src/main.cpp
@@ -13,16 +13,43 @@ int main(int argc, char* argv[])
 {
     loguru::g_stderr_verbosity = loguru::Verbosity_ERROR; // set default verbosity to ERROR or FATAL
     loguru::init(argc, argv); // parse -v flags
-    bool reference_mode = false;
-    std::string peer = "0.0.0.0";
-    uint32_t port = 1337;
+    std::string peer;
+    uint16_t port;
+    uint16_t bind_port;
 
     CLI::App app{"Standalone demo implementation of the Tiny/MiniSync time synchronization algorithms."};
-    app.add_flag("-r,--ref", reference_mode, "Start node in reference mode; "
-                                             "i.e. other peers synchronize to this node's clock.");
-    app.add_option<std::string>("ADDRESS", peer, "Address of peer to synchronize with.", true);
-    app.add_option<uint32_t>("PORT", port, "UDP Port on peer to communicate with.", true);
-    app.add_option("-v", loguru::g_stderr_verbosity, "Set verbosity level.", true);
+
+    auto* ref_mode = app.add_subcommand("REF_MODE", "Start node in reference mode; "
+                                                    "i.e. other peers synchronize to this node's clock.");
+    ref_mode->add_option<uint16_t>("BIND_PORT", bind_port, "Local UDP port to bind to.")->required(true);
+    ref_mode->add_option("-v", loguru::g_stderr_verbosity, "Set verbosity level.", true);
+
+    auto* sync_mode = app.add_subcommand("SYNC_MODE", "Start node in synchronization mode.");
+
+    sync_mode->add_option<uint16_t>("BIND_PORT", bind_port, "Local UDP port to bind to.")->required(true);
+    sync_mode->add_option<std::string>("ADDRESS", peer, "Address of peer to synchronize with.")->required(true);
+    sync_mode->add_option<uint16_t>("PORT", port, "Target UDP Port on peer.")->required(true);
+    sync_mode->add_option("-v", loguru::g_stderr_verbosity, "Set verbosity level.", true);
+
+    app.fallthrough(true);
+    app.require_subcommand(1, 1);
+
     CLI11_PARSE(app, argc, argv)
+
+    auto modes = app.get_subcommands();
+    CHECK_EQ_F(modes.size(), 1, "Wrong number of subcommands - THIS SHOULD NEVER HAPPEN?");
+
+    if (modes.front()->get_name() == "REF_MODE")
+    {
+        LOG_F(INFO, "Started node in REFERENCE mode.");
+    }
+    else if (modes.front()->get_name() == "SYNC_MODE")
+    {
+        LOG_F(INFO, "Started node in SYNCHRONIZATION mode.");
+    }
+    else
+        ABORT_F("Invalid mode specified for application - THIS SHOULD NEVER HAPPEN?");
+
     return 0;
 }
+
