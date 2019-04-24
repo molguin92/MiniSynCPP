706d44ce57d7e3facc4458eef84e355992eafcd6
Manuel Osvaldo Olguín <molguin@kth.se>
2019-04-24 16:16:05 +0200

    more results

--- analysis/analysis.py
+++ analysis/analysis.py
@@ -3,18 +3,31 @@ from matplotlib import pyplot as plt
 
 if __name__ == '__main__':
     df = pandas.read_csv("./stats.csv", sep=';', index_col=False)
-    df = df.drop([0])
-    df.plot(x='Sample', y='Drift', yerr='Drift Error')
+    # df = df.drop([0])
+    # df.plot(x='Sample', y='Drift', yerr='Drift Error')
     fig, ax = plt.subplots()
-    df.plot(x='Sample', y='Offset', yerr='Offset Error', ax=ax)
-    ax.set_yscale('log')
-
-    # plot time vs adjusted time
-    # adj_time = ((df['Drift'].iloc[-1] * df['Timestamp'])
-    #             + df['Offset'].iloc[-1]) / 1000000.0
-    # timestamps = df['Timestamp'] / 1000000.0
-    # fig, ax = plt.subplots()
-    # ax.plot(timestamps, adj_time)
-    # ax.plot(timestamps, timestamps)
-    # ax.ticklabel_format(useOffset=False)
+    ax.plot(df['Sample'], df['Drift'], label='Drift', color='blue')
+    ax.fill_between(df['Sample'],
+                    df['Drift'] - df['Drift Error'],
+                    df['Drift'] + df['Drift Error'],
+                    color='gray', alpha=0.2)
+    ax.legend()
+    ax.set_yscale('symlog', basey=10)
+    ax.set_xscale('symlog', basex=10)
     plt.show()
+    fig.savefig('drift.png')
+
+    fig, ax = plt.subplots()
+    offsets_ms = (df['Offset']) / 1000.0
+    e_offsets_ms = df['Offset Error'] / 1000.0
+
+    ax.plot(df['Sample'], offsets_ms, label='Offset [ms]', color='red')
+    ax.fill_between(df['Sample'],
+                    offsets_ms - e_offsets_ms,
+                    offsets_ms + e_offsets_ms,
+                    color='gray', alpha=0.2)
+    ax.set_yscale('symlog', basey=10)
+    ax.set_xscale('symlog', basex=10)
+    ax.legend()
+    plt.show()
+    fig.savefig('offset.png')
