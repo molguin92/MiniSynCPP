6ff192e9031953098b3438beba6263aac1ca62fe
Manuel Osvaldo Olguín <molguin@kth.se>
2019-05-14 17:03:56 +0200

    update docker_build.sh: only spawn container once.

--- docker_build.sh
+++ docker_build.sh
@@ -1,39 +1,43 @@
 #!/usr/bin/env bash
 
+printf "Building libminisyncpp in a Docker container...\n"
+
 DOCKER_IMG="minisync_build:latest"
 
+printf "Checking if build image exists in registry: "
 if [[ "$(docker images -q ${DOCKER_IMG} 2> /dev/null)" == "" ]]; then
-  # check if image exists. If it doesn't, build it.
-  docker build --tag=${DOCKER_IMG} - < ./Dockerfile
+    # check if image exists. If it doesn't, build it.
+    printf "not found.\n"
+    printf "Building required Docker image:\n"
+    docker build --tag=${DOCKER_IMG} - < ./Dockerfile
+else
+    printf "found.\n"
 fi
 
 LINUX_BUILD_DIR="build_x86_64";
 RASPI_BUILD_DIR="build_ARMv7";
+COMMON_CMAKE_FLAGS="-DCMAKE_BUILD_TYPE=Release \
+-UCMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES \
+-UCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES"
 
 # use the docker image to build both for Linux and for Raspberry Pi
-# Linux:
-docker run --name=linux_build --rm -v ${PWD}:/mnt/build ${DOCKER_IMG} /bin/bash -c \
+printf "Building MiniSynCPP (x86_64 and ARMv7)...\n"
+docker run --name=build --rm -v ${PWD}:/mnt/build ${DOCKER_IMG} /bin/bash -c \
 "\
 cd /mnt/build;\
 mkdir -p ${LINUX_BUILD_DIR};\
 cd ${LINUX_BUILD_DIR};\
-cmake -DCMAKE_BUILD_TYPE=Release -UCMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES -UCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES .. &&\
+cmake ${COMMON_CMAKE_FLAGS} .. &&\
 cmake --build . --target all -- -j 4;\
-./tests/minisyncpp; \
-cd ..; chmod 0777 ${LINUX_BUILD_DIR};
-"
-
-# RaspBerry Pi:
-docker run --name=raspi_build --rm -v ${PWD}:/mnt/build ${DOCKER_IMG} /bin/bash -c \
-"\
-cd /mnt/build;\
+./tests/minisyncpp;\
+cd ..; chmod 0777 ${LINUX_BUILD_DIR};\
 mkdir -p ${RASPI_BUILD_DIR};\
 cd ${RASPI_BUILD_DIR};\
-cmake -DCMAKE_BUILD_TYPE=Release -DCMAKE_C_COMPILER=/opt/cross-pi-gcc-8.3.0-2/bin/arm-linux-gnueabihf-gcc \
+cmake -DCMAKE_C_COMPILER=/opt/cross-pi-gcc-8.3.0-2/bin/arm-linux-gnueabihf-gcc \
 -DCMAKE_CXX_COMPILER=/opt/cross-pi-gcc-8.3.0-2/bin/arm-linux-gnueabihf-g++ \
 -DCMAKE_CXX_FLAGS='-march=armv7-a -mfloat-abi=hard -mfpu=neon-vfpv4' \
--UCMAKE_C_IMPLICIT_INCLUDE_DIRECTORIES -UCMAKE_CXX_IMPLICIT_INCLUDE_DIRECTORIES .. &&\
+${COMMON_CMAKE_FLAGS} .. &&\
 cmake --build . --target all -- -j 4;\
-cd ..; chmod 0777 ${RASPI_BUILD_DIR};
+cd ..; chmod 0777 ${RASPI_BUILD_DIR};\
 "
-
+printf "Done.\n"
